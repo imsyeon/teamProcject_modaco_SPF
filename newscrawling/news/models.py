46392e01f0b4952f0879f152094c6441bd2911fb
imsyeon <86212069+imsyeon@users.noreply.github.com>
2021-07-12 16:10:40 +0900

    newscrawling 파일 삭제

--- newscrawling/news/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class BlogData(models.Model):
-    title = models.CharField(max_length=200)
-    link = models.URLField()
-
-    def __str__(self):
-        return self.title
